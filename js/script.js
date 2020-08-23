07108cf337c1955bc4f7c6f740a8dbdeda54ee44
manoj-kale <54758918+manoj-mk@users.noreply.github.com>
2020-08-23 16:59:50 +0530

    outside click folds nav added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 //------------Nav Bar-------------
 $(document).ready(function(){
+    let hamStatus = true;
     $('#hamburger') .click(function(){
+        hamStatus=false;
         $('.nav-links').toggleClass('height180px');
     })
-
+    $(document).click(function(){
+        if($('.nav-links').hasClass('height180px') && hamStatus){
+            $('.nav-links').removeClass('height180px');
+        }
+        hamStatus=true;
+    })
+    $('.nav-links li a').on('click', function(){
+        $('.nav-links').removeClass('height180px');
+    });
 })
